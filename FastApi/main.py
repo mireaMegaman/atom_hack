c451162dd7839fc373556245458d0399a8ef4c9b
Agar1us <kalinin2003@list.ru>
2023-12-16 19:29:16 +0300

    Update BackEnd

--- FastApi/main.py
+++ FastApi/main.py
@@ -7,18 +7,21 @@ from typing import List
 from random import choice
 from zipfile import ZipFile, ZIP_DEFLATED
 import json
-from ultralytics import YOLO
-
+from ultralytics import YOLO, RTDETR
+from cv2 import imwrite
 from pydantic import BaseModel
 from fastapi import BackgroundTasks, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 
-from ml.main import seed_everything, tracking
+from ml.main import seed_everything, draw_boxes
 
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-app = FastAPI(title="Машинки")
+ml = parent_dir + '/ml/'
+app = FastAPI(title="Обнаружение дефектов на трубах")
 model = None
+CLASS_NAMES_DICT = None
+
 
 class Image64(BaseModel):
     files: List[str]
@@ -48,9 +51,10 @@ app.add_middleware(
 
 @app.on_event("startup")
 def startup_event():
-    global model
+    global model, CLASS_NAMES_DICT
     seed_everything(seed=42)
-    model = YOLO('ml\\yolov8x.pt')
+    model = RTDETR('ml\\best.pt')
+    CLASS_NAMES_DICT = model.model.names
     model.fuse()
 
 
@@ -78,15 +82,17 @@ def image_detection(file: Image64, background: BackgroundTasks):
     path_files = parent_dir + '/photos/'
     images = file.files
     names = file.files_names
-    type_cars = ['Passenger car', 'Motorbike', 'Truck']
+    type_defects = ['Passenger car', 'Motorbike', 'Truck']
     json_ans = {"data": []}
     for i, file in enumerate(images):
         image_as_bytes = str.encode(file)  # convert string to bytes
         img_recovered = base64.b64decode(image_as_bytes)  # decode base64string
         image = Image.open(io.BytesIO(img_recovered))
         _ = image.save(path_files + 'original/' + f"{names[i]}")
-        _ = image.save(path_files + 'results/' + f"{'bbox_'+names[i]}")
-        json_ans['data'].append({'id': i + 1, 'image_path': names[i], 'autotype' : choice(type_cars), 'pollution': 57, 'count': 3})
+        preds = model(path_files + 'original/' + f"{names[i]}")
+        ans = draw_boxes(preds)
+        imwrite(path_files + 'results/' + f"boxed_image-{names[i]}", ans)
+        json_ans['data'].append({'id': i + 1, 'image_path': names[i], 'autotype' : [''], 'pollution': '108, 36', 'count': 3})
     with open(path_files + 'results/' + 'data.txt', 'w') as outfile:
         json.dump(json_ans, outfile)
     background.add_task(remove_file, path_files + '/results/')
@@ -94,41 +100,18 @@ def image_detection(file: Image64, background: BackgroundTasks):
 
 
 # @app.post('/video')
-# def video_traking(input: Video, background: BackgroundTasks):
-#     image_as_bytes = str.encode(input.file)
-#     img_recovered = base64.b64decode(image_as_bytes)
-#     image = Image.open(io.BytesIO(img_recovered))
-#     input_file_path = parent_dir + '/videos/original/' + f"{input.name}.mp4"
-#     _ = image.save(input_file_path)
+# def video_traking(input: Video):
+#     json_ans = {"data": [{'id': 1, 'image_path': input.file, 'autotype' : 'car', 'pollution': 57, 'count': 3}]}
 #     output_path_file = parent_dir + '/videos/results/' + f"result_{input.file}.mp4"
 #     tracking(
 #         model=model,
 #         conf=0.4,
-#         input_filename=input_file_path,
+#         input_filename=input.file,
 #         output_filename=output_path_file
 #     )
-#     background.add_task(remove_file, parent_dir + '/videos/results/')
+#     with open(parent_dir + '/videos/results/' + 'data.txt', 'w') as outfile:
+#         json.dump(json_ans, outfile)
 #     return to_zip(parent_dir + '/videos/results/')
-
-
-@app.post('/video')
-def video_traking(input: Video):
-    json_ans = {"data": [{'id': 1, 'image_path': input.file, 'autotype' : 'car', 'pollution': 57, 'count': 3}]}
-    output_path_file = parent_dir + '/videos/results/' + f"result_{input.file}.mp4"
-    tracking(
-        model=model,
-        conf=0.4,
-        input_filename=input.file,
-        output_filename=output_path_file
-    )
-    with open(parent_dir + '/videos/results/' + 'data.txt', 'w') as outfile:
-        json.dump(json_ans, outfile)
-    return to_zip(parent_dir + '/videos/results/')
-
-
-@app.post('/rtsp')
-def online_tracking(input: RTSP):
-    return {"message": "Something"}
 
 
 @app.post('/active_learning')
